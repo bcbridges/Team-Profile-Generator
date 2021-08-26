ac993e2ac6d0fc015a994d6296f65b2203cf2ee2
bcbridges <bcb35b@gmail.com>
2021-08-25 20:13:47 -0600

    Updated subclass js files to contain a module export. Added Inquirer prompt questions to index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const inquirer = require("inquirer");
+// const Employee = require("./lib/employee"); -- parent class
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
 inquirer
   .prompt({
@@ -8,17 +12,119 @@ inquirer
     choices: ["Manager", "Engineer", "Intern", "I'm Done"],
   })
   .then((response) => {
-    switch (response.emplType) {
-      case "Manager":
-        console.log("Manager Test");
-        break;
-      case "Engineer":
-        console.log("Engineer Test");
-        break;
-      case "Intern":
-        console.log("Intern Test");
-        break;
-      default:
-        console.log("This is the default");
+    if (response.emplType == "I'm Done") {
+      //Add if index.html doesnt exist clause unless there's already data there.
+      console.log(
+        "You're all set! You can find your html file here - ./dist/index.html"
+      );
+    } else {
+      switch (response.emplType) {
+        case "Manager":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What is the Manager's name?",
+                name: "name",
+              },
+              {
+                type: "input",
+                message: "What is the Manager's ID number?",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "What is the Manager's email?",
+                name: "email",
+              },
+              {
+                type: "input",
+                message: "What is the Manager's office number?",
+                name: "officeNum",
+              },
+            ])
+            .then((response) => {
+              const managerData = new Manager(
+                response.name,
+                response.id,
+                response.email,
+                response.officeNum
+              );
+            });
+          break;
+        case "Engineer":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What is the Engineer's name?",
+                name: "name",
+              },
+              {
+                type: "input",
+                message: "What is the Engineer's ID number?",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "What is the Engineer's email?",
+                name: "email",
+              },
+              {
+                type: "input",
+                message: "What is the Engineer's GitHub username?",
+                name: "github",
+              },
+            ])
+            .then((response) => {
+              const engineerData = new Engineer(
+                response.name,
+                response.id,
+                response.email,
+                response.github
+              );
+            });
+          break;
+        case "Intern":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What is the Intern's name?",
+                name: "name",
+              },
+              {
+                type: "input",
+                message: "What is the Intern's ID number?",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "What is the Intern's email?",
+                name: "email",
+              },
+              {
+                type: "input",
+                message: "What school is the intern from?",
+                name: "school",
+              },
+            ])
+            .then((response) => {
+              const internData = new Intern(
+                response.name,
+                response.id,
+                response.email,
+                response.github
+              );
+            });
+          break;
+        default:
+          console.log("This is the default - an error has occured");
+      }
     }
   });
+
+//function to ask all typical questions
+const employeeQuestions = () => {
+  return;
+};
