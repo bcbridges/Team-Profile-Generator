4ba0f90ef0d99bfde8f55cb3c1841e4a830c0440
bcbridges <bcb35b@gmail.com>
2021-08-26 00:30:13 -0600

    Added test for the Intern subclass.

---
+++ tests/Intern.test.js
@@ -0,0 +1,29 @@
+const Engineer = require("../lib/intern");
+
+describe("getSchool", () => {
+  it("should return the school key value as a string", () => {
+    const str = "Missouri S&T";
+    const result = new Intern(
+      "Brice",
+      1,
+      "bcb35b@gmail.com",
+      "Missouri S&T"
+    ).getSchool();
+
+    expect(result).toEqual(str);
+  });
+});
+
+describe("getRole", () => {
+  it("should return the role key value as a string", () => {
+    const str = "Intern";
+    const result = new Intern(
+      "Brice",
+      1,
+      "bcb35b@gmail.com",
+      "Missouri S&T"
+    ).getRole();
+
+    expect(result).toEqual(str);
+  });
+});
